b6cfa512a96084e41bd22c3e43514c03ed2662c0
Emilie <emilie.chnvt@gmail.com>
2025-05-23 06:13:45 +0200

    <meta name="turbo-prefetch" content="false">
     disallow pre fetched

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,28 +3,33 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Repository\CategoryRepository;
 use App\Service\CartService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class CartController extends AbstractController
 {
     #[Route('/cart', name: 'app_cart')]
-    public function index(CartService $cartService): Response
+    public function index(CartService $cartService, CategoryRepository $categoryRepository): Response
     {
         return $this->render('cart/index.html.twig', [
             'cart'=>$cartService->getCart(),
             'total'=>$cartService->getTotalPrice(),
+           'categories'=>$categoryRepository->findAll(),
            // 'count'=>$cartService->getCount(),
 
         ]);
     }
 
 
-    #[Route('/cart/add/{id}/{quantity}', name: 'app_cart_add')]
-    public function add(CartService $cartService, Product $product, int $quantity): Response
+    #[Route('/cart/add/{id}/', name: 'app_cart_add')]
+    public function add(CartService $cartService, Product $product, Request $request): Response
     {
+        $quantity = (int) $request->request->get('quantity', 1);
+
         if(!$this->getUser()){return $this->redirectToRoute('app_login');}
         $cartService->addToCart($product, $quantity);
         return $this->redirectToRoute('app_cart');
