ff1ec37c23846bd552e84b4ed15f69c6e8cb4795
Emilie <emilie.chnvt@gmail.com>
2025-05-28 15:39:23 +0200

    google in register/login added

--- src/Controller/GoogleController.php
+++ src/Controller/GoogleController.php
@@ -1,59 +1,29 @@
 <?php
 
+// src/Controller/GoogleController.php
 
 namespace App\Controller;
 
-
-
-use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
-
-
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
-
-use Symfony\Component\HttpFoundation\Request;
-
-
-
-use Symfony\Component\Routing\Annotation\Route;
-
-
-use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
-
-
 use App\Entity\User;
-
-
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-
-
+use App\Entity\Profile;
+use App\Security\SecurityAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
-
-
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
-
-use App\Security\SecurityAuthenticator;
-
-
 class GoogleController extends AbstractController
-
-
 {
-
-
     #[Route('/connect/google', name: 'connect_google_start')]
-
     public function connectAction(ClientRegistry $clientRegistry)
-
-
     {
         if ($this->getUser()) {
             return $this->redirectToRoute('app_products');
-
-
         }
-
 
         return $clientRegistry
             ->getClient('google')
@@ -61,141 +31,59 @@ class GoogleController extends AbstractController
                 'prompt' => 'select_account',
                 'scope' => ['email', 'profile'],
             ]);
-
-
     }
 
-
     #[Route('/connect/google/check', name: 'connect_google_check')]
-
-
     public function connectCheckAction(
-
-
         Request $request,
-
-
         ClientRegistry $clientRegistry,
-
-
         UserPasswordHasherInterface $userPasswordHasher,
-
-
         EntityManagerInterface $entityManager,
-
-
         UserAuthenticatorInterface $userAuthenticator,
-
-
-        SecurityAuthenticator $authenticator)
-
-
-    {
-
-
+        SecurityAuthenticator $authenticator
+    ) {
         if ($this->getUser()) {
-
-
             return $this->redirectToRoute('app_products');
-
-
         }
-
 
         $client = $clientRegistry->getClient('google');
 
-
         try {
-
-
-            $facebookUser = $client->fetchUser();
-
-
-// check if email exist
-
+            $googleUser = $client->fetchUser();
 
             $existingUser = $entityManager->getRepository(User::class)
+                ->findOneBy(['email' => $googleUser->getEmail()]);
 
-
-                ->findOneBy(['email' => $facebookUser->getEmail()]);
-
-
-            if($existingUser){
-
-
+            if ($existingUser) {
                 return $userAuthenticator->authenticateUser(
-
-
                     $existingUser,
-
-
                     $authenticator,
-
-
                     $request
-
-
                 );
-
-
             }
 
-
             $user = new User();
-
-
+            $user->setEmail($googleUser->getEmail());
             $user->setPassword(
-
-
-                $userPasswordHasher->hashPassword(
-
-
-                    $user,
-
-
-                    $facebookUser->getId()
-
-
-                )
-
-
+                $userPasswordHasher->hashPassword($user, $googleUser->getId())
             );
-
-
-            $user->setEmail($facebookUser->getEmail());
-
 
             $entityManager->persist($user);
 
+            $profile = new Profile();
+            $profile->setOfUser($user);
 
+            $entityManager->persist($profile);
             $entityManager->flush();
 
-
             return $userAuthenticator->authenticateUser(
-
-
                 $user,
-
-
                 $authenticator,
-
-
                 $request
-
-
             );
-
-
         } catch (IdentityProviderException $e) {
-
-
-            var_dump($e->getMessage()); die;
-
-
+            dump($e->getMessage());
+            die;
         }
-
-
     }
-
-
 }
